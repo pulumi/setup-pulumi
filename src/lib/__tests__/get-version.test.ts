8e00413bef3e402040a157daffa3f1937dd84cc4
Simen A. W. Olsen <so@bjerk.io>
2021-04-24 13:23:00 +0200

    wip

--- src/lib/__tests__/get-version.test.ts
+++ src/lib/__tests__/get-version.test.ts
@@ -3,11 +3,17 @@ import nock from "nock";
 
 const nockBack = nock.back;
 nockBack.fixtures = `${__dirname}/../__fixtures__`;
+nockBack.setMode("record");
 
 describe("get-version", () => {
+  let nockDone: () => void;
   beforeEach(async () => {
-    process.env.GITHUB_TOKEN = "my-token";
-    await nockBack("get-version.json");
+    process.env.GITHUB_TOKEN = process.env.GITHUB_TOKEN || "my-token";
+    const nb = await nockBack("get-version.json");
+    nockDone = nb.nockDone;
+  });
+  afterEach(async () => {
+    await nockDone();
   });
   it("should match ^3 versions", async () => {
     const version = await getSatisfyingVersion("^3");
@@ -21,6 +27,10 @@ describe("get-version", () => {
     const version = await getSatisfyingVersion("3.0.*");
     expect(version).toMatchInlineSnapshot(`"v3.0.0"`);
   });
+  it("should match v1.*.* versions", async () => {
+    const version = await getSatisfyingVersion("v1.*.*");
+    expect(version).toMatchInlineSnapshot(`"v1.14.1"`);
+  });
   it("should match v2.22.* versions", async () => {
     const version = await getSatisfyingVersion("v2.22.*");
     expect(version).toMatchInlineSnapshot(`"v2.22.0"`);
@@ -32,5 +42,9 @@ describe("get-version", () => {
   it("should match v2.17.2 version", async () => {
     const version = await getSatisfyingVersion("v2.17.2");
     expect(version).toMatchInlineSnapshot(`"v2.17.2"`);
+  });
+  it("should match v2.5.0 version", async () => {
+    const version = await getSatisfyingVersion("2.5.0");
+    expect(version).toMatchInlineSnapshot(`"v2.5.0"`);
   });
 });
--- src/lib/get-version.ts
+++ src/lib/get-version.ts
@@ -1,19 +1,22 @@
 import * as core from "@actions/core";
 import { getOctokit } from "@actions/github";
+import { OctokitOptions } from "@octokit/core/dist-types/types";
 import { maxSatisfying } from "semver";
 
 export async function getSatisfyingVersion(
   range: string
 ): Promise<string | null> {
   const octokit = getOctokit(
-    core.getInput("github-token") || process.env.GITHUB_TOKEN || ''
+    core.getInput("github-token") || process.env.GITHUB_TOKEN || ""
   );
-  const releases = await octokit.repos.listReleases({
+  
+  const releases = await octokit.paginate(octokit.rest.repos.listTags, {
     repo: "pulumi",
     owner: "pulumi",
+    per_page: 100,
   });
 
-  const versions = releases.data.map((r) => r.tag_name);
+  const versions: string[] = releases.map((r) => r.name);
 
   return maxSatisfying(versions, range);
 }
